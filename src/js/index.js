efc9e7b7547652982c232612037b47a09882abf6
Fernanda Sampaio <fernanda@MacBook-Air-de-Fernanda.local>
2019-12-19 09:58:38 -0300

    Domain-Generation

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,20 @@ import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
+
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  let ul = document.querySelector("#domains");
+  let a = "";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let x = 0; x < noun.length; x++) {
+        console.log(pronoun[i] + adj[j] + noun[x] + ".com");
+        a += "<li>" + pronoun[i] + adj[j] + noun[x] + ".com" + "</li>";
+      }
+    }
+  }
+  ul.innerHTML = a;
 };
